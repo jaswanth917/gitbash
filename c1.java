e1f5aca842a53ed3532f181a9dcfcd7f0adde971
sai tharun <boddusaitarun2002@gmail.com>
2022-11-03 11:23:30 +0530

    first commit

---
+++ c1.java
@@ -0,0 +1,9 @@
+public class IfExample {  
+public static void main(String[] args) {   
+    int age=20;   
+    if(age>18){  
+        System.out.print("Age is greater than 18");  
+    }  
+}  
+}  [A[A[B[B
+[A[A[A[B[C[C[C[C[C[C[C[C[C[C[C[Cc1.java
